c89751852e2f1247e12fb3e898d2d9fb6aec1977
yuchuankai <yuchuankai@bjsasc.com>
2024-04-07 16:55:53 +0800

    [featurel]: 流的简单应用

--- src/main/java/流应用/FilterFormStream.java
+++ src/main/java/流应用/FilterFormStream.java
@@ -1,29 +1,28 @@
 /**
  * @Project: testDeomInSpring
- * @ClassName: main
- * @Date: 2024年 04月 07日 15:56
+ * @ClassName: FilterFormStream
+ * @Date: 2024年 04月 07日 16:49
  * @version V1.0
  * @Author: MR.Yu
  * Copyright (c) All Rights Reserved, 2024.
  **/
 package 流应用;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
  * @Description:
- * @Date: 2024年 04月 07日 15:56
+ * @Date: 2024年 04月 07日 16:49
  * @Author: MR.Yu
  **/
-public class main {
+public class FilterFormStream {
 
     public static void main(String[] args) {
         List<String> list = Arrays.asList("1","2","3","4","5");
-
-        List<String> collect = list.stream().map(s -> s + "1").collect(Collectors.toList());
+        List<String> collect = list.stream().filter(s -> Integer.parseInt(s) % 2 == 0).collect(Collectors.toList());
         collect.stream().forEach(System.out::println);
+
     }
 }
